b8c4f43b99c1bd6e80c67b7cd2ab328c0ebe3c13
Flavio Carmona <32350974+fcarmonab@users.noreply.github.com>
2022-03-15 09:03:49 -0600

    Modificaciones al MainPage

--- EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/MainPage.xaml.cs
+++ EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/MainPage.xaml.cs
@@ -5,14 +5,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using EjercicioDosPuntoCuatro.Views;
+using Xam.Forms.VideoPlayer;
+using Xamarin.Essentials;
+using System.IO;
+
 
 namespace EjercicioDosPuntoCuatro
 {
     public partial class MainPage : ContentPage
     {
+        public string PhotoPath;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void btnVideo_Clicked(object sender, EventArgs e)
+        {
+            TomarVideoDeGaleria();
+        }
+
+        public async void TomarVideoDeGaleria()
+        {
+            try
+            {
+                var photo = await MediaPicker.PickVideoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+                //await DisplayAlert("as", PhotoPath, "ok");
+
+                UriVideoSource uriVideoSurce = new UriVideoSource()
+                {
+                    Uri = PhotoPath
+                };
+                videoPlayer.Source = uriVideoSurce;
+            }
+
+            catch (FeatureNotSupportedException)
+            {
+            }
+            catch (PermissionException)
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        public async void TomarVideoTiempoReal()
+        {
+            try
+            {
+                var photo = await MediaPicker.CaptureVideoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+                //await DisplayAlert("as", PhotoPath, "ok");
+
+                UriVideoSource uriVideoSurce = new UriVideoSource()
+                {
+                    Uri = PhotoPath
+                };
+
+                videoPlayer.Source = uriVideoSurce;
+            }
+            catch (FeatureNotSupportedException)
+            {
+            }
+            catch (PermissionException)
+            {
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+            await stream.CopyToAsync(newStream);
+            PhotoPath = newFile;
+        }
+
+        private void videoPlayer_PlayCompletion(object sender, EventArgs e)
+        {
+        }
+
+        private void btnreproducir_Clicked(object sender, EventArgs e)
+        {
+            TomarVideoTiempoReal();
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Guardar_Datos();
+        }
+
+        public async void Guardar_Datos()
+        {
+            var videos = new Models.Videos
+            {
+                Url_video = PhotoPath,
+                Descripcion = txtPineda.Text
+            };
+
+            var resultado = await App.BaseDatos.GrabarVideos(videos);
+
+            if (resultado == 1)
+            {
+                await DisplayAlert("Mensaje", "Video Registrado.", "ok");               
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo Guardar", "ok");
+            }
+        }
+
+        private async void btnLista_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListVideoPage());
         }
     }
 }
--- EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,32 @@ namespace EjercicioDosPuntoCuatro {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xam.Forms.VideoPlayer.VideoPlayer videoPlayer;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnVideo;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnreproducir;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry txtPineda;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnGuardar;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnLista;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            videoPlayer = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xam.Forms.VideoPlayer.VideoPlayer>(this, "videoPlayer");
+            btnVideo = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnVideo");
+            btnreproducir = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnreproducir");
+            txtPineda = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "txtPineda");
+            btnGuardar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnGuardar");
+            btnLista = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnLista");
         }
     }
 }
