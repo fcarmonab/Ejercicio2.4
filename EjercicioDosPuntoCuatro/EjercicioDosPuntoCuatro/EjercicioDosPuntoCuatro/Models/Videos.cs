5415614682106d2765b33a7d783ec704c586b781
Flavio Carmona <32350974+fcarmonab@users.noreply.github.com>
2022-03-15 08:07:51 -0600

    Creacion de Modelo: clase Videos
    Dentro de carpeta Models

---
+++ EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/EjercicioDosPuntoCuatro/Models/Videos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace EjercicioDosPuntoCuatro.Models
+{
+    public class Videos
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Codigo { get; set; }
+
+        [MaxLength(200)]
+        public string Url_video { get; set; }
+
+        [MaxLength(80)]
+        public string descripcion { get; set; }
+    }
+}
